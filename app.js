c1fa82739a9333b593f2697932a03d1e4a94078a
Claudio <cjnetbr@gmail.com>
2025-10-03 10:03:15 -0300

    02. Funções - funções com retorno

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Numero Secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do Numero Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero entre 0 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um numero entre 0 e 10';
+
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
 function verificarChute(){
     console,console.log('O botão foi clicado');
-    
 }
+
+exibirTextoNaTela('h1', 'Jogo do Numero Secreto');
+exibirTextoNaTela('p', 'Escolha um numero entre 0 e 100');
+
+
